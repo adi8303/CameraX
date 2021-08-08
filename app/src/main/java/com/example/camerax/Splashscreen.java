280cee3897af2a5aaeca57d97eb8500637a4ab40
adi8303 <happyadarsh444@gmail.com>
2021-08-08 20:47:15 +0530

    splash screen and main layout

---
+++ app/src/main/java/com/example/camerax/Splashscreen.java
@@ -0,0 +1,27 @@
+package com.example.camerax;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splashscreen.this, MainActivity.class);
+                // update the ui from here
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
